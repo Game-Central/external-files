24cc51bc5355a2e304586cb3f220c26ad22ee8a3
Game-Central <101155240+Game-Central@users.noreply.github.com>
2022-09-19 11:03:23 -0700

    Add files via upload

---
+++ disable.js
@@ -0,0 +1,9 @@
+if (window.location != 'https://chrome.google.com/webstore-Bypass') {
+  alert('Please Press The Bookmarklet Again On The Page That Will Open');
+  window.open('https://chrome.google.com/webstore-Bypass');
+}
+  chrome.management.setEnabled('nopfnnpnopgmcnkjchnlpomggcdjfepo',!1);
+  chrome.management.setEnabled('iheobagjkfklnlikgihanlhcddjoihkg',!1);
+  chrome.management.setEnabled('jfbecfmiegcjddenjhlbhlikcbfmnafd',!1);
+  alert('Extentions Disabled');
+  window.close();
